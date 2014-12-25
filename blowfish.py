2377aa0b2d047aa1c9638e5a7a5a86042d87d594
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2014-12-24 20:39:39 -0800

    Inline decryption rounds in decrypt_cbc

--- blowfish.py
+++ blowfish.py
@@ -597,24 +597,31 @@ class Cipher(object):
     If it is not, a :exc:`struct.error` exception is raised.
     """
     S0, S1, S2, S3 = self.S
-    P_reversed = self.P_reversed
+    P = self.P
     u4_2_pack = self._u4_2_pack
-    cycle_fast = self._cycle_fast
     u1_4_unpack = self._u1_4_unpack
     u4_1_pack = self._u4_1_pack
+    
+    p_first, p_second = P[0]
     
     prev_cipher_L, prev_cipher_R = self._u4_2_unpack(init_vector)
     
     for cipher_L, cipher_R in self._u4_2_iter_unpack(data):
-      L, R = cycle_fast(
-        cipher_L,
-        cipher_R,
-        P_reversed, S0, S1, S2, S3,
-        u1_4_unpack, u4_1_pack
-      )
+      L = cipher_L
+      R = cipher_R
+      for p2, p1 in P[:0:-1]:
+        L ^= p1
+        a, b, c, d = u1_4_unpack(u4_1_pack(L))
+        R ^= (S0[a] + S1[b] ^ S2[c]) + S3[d] & 0xffffffff
+        R ^= p2
+        a, b, c, d = u1_4_unpack(u4_1_pack(R))
+        L ^= (S0[a] + S1[b] ^ S2[c]) + S3[d] & 0xffffffff
+      L, R = R ^ p_first, L ^ p_second
+      
       yield u4_2_pack(prev_cipher_L ^ L, prev_cipher_R ^ R)
-      prev_cipher_L, prev_cipher_R = cipher_L, cipher_R
-  
+      prev_cipher_L = cipher_L
+      prev_cipher_R = cipher_R
+      
   def encrypt_pcbc(self, data, init_vector):
     """
     Return an iterator that encrypts `data` using the Propagating Cipher-Block
