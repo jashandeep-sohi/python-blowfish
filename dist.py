90e950b72248be409302dc11014179522487b0d2
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2015-10-04 20:40:21 -0700

    Add support for cythonizing extensions through the 'build_ext' command.

--- dist.py
+++ dist.py
@@ -19,8 +19,100 @@
 import unittest
 import sys
 import os.path
+from pathlib import Path
+from distutils.command.build_ext import build_ext as org_build_ext
 from distutils.cmd import Command
-from distutils.errors import DistutilsOptionError
+from distutils.extension import Extension
+from distutils.errors import DistutilsOptionError, DistutilsFileError
+
+try:
+  from Cython.Compiler.Main import compile_single, CompilationOptions
+except ImportError:
+  HAVE_CYTHON = False
+else:
+  HAVE_CYTHON = True
+
+
+class CythonExtension(Extension):
+
+  def __init__(
+    self,
+    name,
+    cython_source,
+    sources = None,
+    output_dir = None,
+    language_level = None,
+    cplus = None,
+    annotate = None,
+    **kwargs
+  ):
+    self.cython_source = Path(cython_source)
+    self.output_dir = output_dir or Path("cythonized")
+    self.language_level = language_level or 3
+    self.cplus = cplus or False
+    self.annotate = annotate or False
+    
+    self.output_file = self.output_dir.joinpath(
+      self.cython_source.with_suffix(".cpp" if self.cplus else ".c")
+    )
+    
+    sources = sources or []
+    sources.append(str(self.output_file))
+    super().__init__(name, sources, **kwargs)
+    
+
+class build_ext(org_build_ext):
+  
+  def check_newer(self, source, target):
+    if not target.is_file():
+      return True
+    
+    if source.stat().st_mtime > target.stat().st_mtime:
+      return True
+    
+    return False
+  
+  def build_extensions(self):
+    for ext in self.extensions:
+      if not isinstance(ext, CythonExtension):
+        continue
+      self.cythonize_extension(ext)
+    
+    super().build_extensions()
+  
+  def cythonize_extension(self, ext):
+    if not HAVE_CYTHON:
+      if not ext.output_file.is_file():
+        raise DistutilsFileError(
+          "expected cythonized file %r not found" % str(ext.output_file)
+        )
+      else:
+        return
+    
+    is_newer = self.check_newer(ext.cython_source, ext.output_file)
+    if not self.force and not is_newer:
+      return
+    
+    self.announce("cythonizing %r extension" % ext.name, 2)
+    
+    try:
+      ext.output_dir.mkdir(parents=True)
+    except FileExistsError:
+      pass
+    
+    options = CompilationOptions(
+      defaults = None,
+      output_file = str(ext.output_file),
+      language_level = ext.language_level,
+      cplus = ext.cplus,
+      annotate = ext.annotate,
+    )
+    source = str(ext.cython_source)
+    output_file = str(ext.output_file)
+    
+    self.announce("cythonizing %r -> %r" % (source, output_file), 2)
+    result = compile_single(source, options, ext.name)
+  
 
 class test(Command):
   
@@ -46,7 +138,7 @@ class test(Command):
     self.catch = False
     self.failfast = False
     self.exit = True
-    
+  
   def finalize_options(self):    
     if self.test_name and (self.start_dir or self.pattern):
       raise DistutilsOptionError(
@@ -58,7 +150,7 @@ class test(Command):
     
     if self.pattern is None:
       self.pattern = "test*.py"
-      
+  
   def run(self):    
     build = self.get_finalized_command("build")
     build.run()
@@ -87,4 +179,4 @@ class test(Command):
     
     if self.exit and not test_results.wasSuccessful():
       raise SystemExit()
-      
+  
